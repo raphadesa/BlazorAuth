416cfd6d79ed22df586933bf0a45398016823990
Raphaël Désalbres <raphael.desalbres@outlook.com>
2022-06-10 13:28:20 +0200

    Ajoutez des fichiers projet.

---
+++ Pages/Authenticate.cshtml.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
+
+namespace BlazorAuth.Pages
+{
+
+    [AllowAnonymous]
+    public partial class Authenticate : PageModel
+    {
+        public async Task<IActionResult> OnGetAsync(string user)
+        {
+            try
+            {
+                // Clear the existing external cookie
+                await HttpContext
+                    .SignOutAsync(
+                    CookieAuthenticationDefaults.AuthenticationScheme);
+            }
+            catch { }
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user)
+            };            
+
+            var claimsIdentity = new ClaimsIdentity(
+                claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var authProperties = new AuthenticationProperties
+            {
+                IsPersistent = true,
+                RedirectUri = this.Request.Host.Value
+            };            
+
+            try
+            {
+                await HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity),
+                authProperties);
+            }
+            catch (Exception ex)
+            {
+                string error = ex.Message;
+            }
+
+            return LocalRedirect(Url.Content("~/"));
+        }
+
+    }
+}
+
+
---
+++ ViewModels/LoginVM.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorAuth.ViewModels
+{
+    public class LoginVM
+    {
+        [Required(ErrorMessage = "Please provide your login !")]
+        public string login { get; set; } = "johndoe@contoso.com";
+        [Required(ErrorMessage = "Please provide your password !")]
+        public string password { get; set; } = "test";
+    }
+}
